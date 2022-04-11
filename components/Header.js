beee0870e79146e7c31eb2e710de895278c1edb0
Ezequiel de Valais <nicounyi@gmail.com>
2022-04-11 00:21:44 -0300

    estructura

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+
+const SearchBar = () => {
+  return (
+    <>
+      <header className="container-fluid">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+                
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default SearchBar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/styles/main.scss"
+import Head from "next/head";
+import { useRouter } from "next/dist/client/router";
+import Layout from "../components/layout";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>RipioScan</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
